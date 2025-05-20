186728408208e40b084dd1d1fe2de96ded6ccc17
devpull <devpull@yandex.ru>
2025-05-20 17:25:28 +0300

    Left config with netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   site: "https://artbrains.ru",
   trailingSlash: "always",
 
-  // adapter: netlify(),
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: netlify(),
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
 });
